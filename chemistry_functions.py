f95da4b55b6200859ae9d3c61cc8148d52f5a017
xarDeves <ch.nteves@gmail.com>
2025-03-28 17:51:47 +0200

    split up some classes and constants to files

--- chemistry_functions.py
+++ chemistry_functions.py
@@ -1,27 +1,10 @@
 from io import BytesIO
 
-from PyQt5.QtGui import QPixmap, QImage, QIcon
+from PyQt5.QtGui import QPixmap, QImage
 from rdkit import Chem
 from rdkit.Chem import Draw, rdMolDescriptors
 
-CATEGORY_SMARTS = {
-    "Aldehyde": "[CX3H1](=O)[#6]",
-    "Primary amines": "[NX3H2][#6]",
-    "Secondary amines": "[NX3H1]([#6])[#6]",
-    "Tertiary amines": "[NX3]([#6])([#6])[#6]",
-    "Alcohols": "[OX2H][#6]",
-    "Esters": "[CX3](=O)O[CX4]",
-    "Ethers": "[OX2]([CX4])[CX4]",
-    "Carboxylic acids": "[CX3](=O)O[HX1]",
-    "Amides": "[NX3][CX3](=O)[#6]",
-    "Aromatic": "[c]",
-    "Cyclic": "[#6]1[#6]2[#6]3[#6]4[#6]51",
-    "Heterocyclic": "[!#6]",
-    "Linear": "[#6]-*-[#6]",
-    "Boronic acids/esters": "[$([B](O)(O)),$([B](OC(C)(C)C)(OC(C)(C)C))]"
-}
-
-IMAGE_SIZE = 512
+from app_constants import *  # noqa
 
 
 def calculate_formula(smiles):
